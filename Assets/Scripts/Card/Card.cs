59cc95a2899c38a7828366c7283ad5fe1cd3ecfb
Sainori <al.podkolzin.0@gmail.com>
2020-11-18 20:29:42 +0300

    Add first card opening

--- Assets/Scripts/Card/Card.cs
+++ Assets/Scripts/Card/Card.cs
@@ -1,17 +1,24 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 namespace Card
 {
     public class Card : MonoBehaviour, ICard
     {
+        private const string Opened = "Opened";
+        private const string Closed = "Closed";
+
+        private static readonly int IsOpened = Animator.StringToHash("IsOpened");
+
+        [SerializeField] private float selectDuration = 1;
+        [SerializeField] private float firstShowDuration = 3;
+
         [SerializeField] private uint _cardType;
         [SerializeField] private GameObject cardModel;
 
         private Animator _animator;
-
-        private static readonly int IsOpened = Animator.StringToHash("IsOpened");
-        private static readonly int OpeningState = Animator.StringToHash("Opening");
-        private static readonly int ClosingState = Animator.StringToHash("Closing");
+        private Coroutine _opening;
 
         private void Awake()
         {
@@ -22,11 +29,35 @@ namespace Card
         {
             _cardType = cardType;
             cardModel.transform.GetComponent<MeshRenderer>().material.color = new Color(0, 0, (float) cardType / 4);
+
+            StartCoroutine(OpenCard(firstShowDuration));
         }
 
         public void SelectCard()
         {
-            _animator.SetBool(IsOpened, !_animator.GetBool(IsOpened));
+            _opening = StartCoroutine(OpenCard(selectDuration));
+        }
+
+        private IEnumerator OpenCard(float duration)
+        {
+            Debug.Log("Open card" + duration);
+
+            var stateInfo = _animator.GetCurrentAnimatorStateInfo(0);
+            if (!stateInfo.IsName(Closed))
+            {
+                Debug.Log("break");
+                yield break;
+            }
+
+            _animator.SetBool(IsOpened, true);
+
+            if (!stateInfo.IsName(Opened))
+            {
+                yield return null;
+            }
+
+            yield return new WaitForSeconds(duration);
+            _animator.SetBool(IsOpened, false);
         }
     }
 }
