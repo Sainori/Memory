cf23f24ddcefac73fde432481d7b03c7c2e0cce9
Sainori <al.podkolzin.0@gmail.com>
2020-11-20 20:24:22 +0300

    Add touch input

--- Assets/Scripts/InputSystem/InputSystem.cs
+++ Assets/Scripts/InputSystem/InputSystem.cs
@@ -5,16 +5,65 @@ namespace InputSystem
 {
     public class InputSystem : MonoBehaviour, IInputSystem
     {
-        //TODO: implement touch for smartphone
         public void DirectUpdate()
         {
-            var touch = Input.GetMouseButtonUp(0);
-            if (!touch)
+            if (!TryGetInputPosition(out var inputPosition))
             {
                 return;
             }
 
-            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            CastRay(inputPosition);
+        }
+
+        private bool TryGetInputPosition(out Vector3 inputPosition)
+        {
+            switch (Application.platform)
+            {
+                case RuntimePlatform.WindowsPlayer:
+                case RuntimePlatform.WindowsEditor:
+                case RuntimePlatform.OSXEditor:
+                case RuntimePlatform.OSXPlayer:
+                    return TryGetMouseClick(out inputPosition);
+
+                case RuntimePlatform.Android:
+                case RuntimePlatform.IPhonePlayer:
+                    return TryGetTouch(out inputPosition);
+
+                default:
+                    inputPosition = default;
+                    return false;
+            }
+        }
+
+        private bool TryGetTouch(out Vector3 inputPosition)
+        {
+            var touch = Input.GetTouch(0);
+            if (touch.phase != TouchPhase.Ended)
+            {
+                inputPosition = default;
+                return false;
+            }
+
+            inputPosition = touch.position;
+            return true;
+        }
+
+        private bool TryGetMouseClick(out Vector3 inputPosition)
+        {
+            var mouseButtonUp = Input.GetMouseButtonUp(0);
+            if (!mouseButtonUp)
+            {
+                inputPosition = default;
+                return false;
+            }
+
+            inputPosition = Input.mousePosition;
+            return true;
+        }
+
+        private void CastRay(Vector3 position)
+        {
+            var ray = Camera.main.ScreenPointToRay(position);
             RaycastHit hit;
             if (!Physics.Raycast(ray, out hit))
             {
