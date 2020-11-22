b4c9072467f6129cbf3e82f311f67c161424d1fe
Sainori <al.podkolzin.0@gmail.com>
2020-11-23 01:52:17 +0300

    Added camera help const

--- Assets/Scripts/LightAndCameraSystem/LightAndCameraSystem.cs
+++ Assets/Scripts/LightAndCameraSystem/LightAndCameraSystem.cs
@@ -4,18 +4,22 @@ namespace LightAndCameraSystem
 {
     public class LightAndCameraSystem : MonoBehaviour, ILightAndCameraSystem
     {
-        [SerializeField] private Transform mainCamera;
+        [SerializeField] private float aspectConst = 33.75f;
         [SerializeField] private float distanceBetweenFieldAndCamera;
+
+        [SerializeField] private Camera mainCamera;
+        [SerializeField] private Transform cameraTransform;
         [SerializeField] private Vector3 additionalRotation = new Vector3(0, -90, 0);
 
         [SerializeField] private Transform lightOnScene;
 
         public void Initiallize(Vector3 fieldCenter)
         {
-            mainCamera.position = new Vector3(fieldCenter.x, fieldCenter.y + distanceBetweenFieldAndCamera, fieldCenter.z);
-            var toFieldDirection = (fieldCenter - mainCamera.position).normalized;
+            mainCamera.fieldOfView = aspectConst / mainCamera.aspect;
+            cameraTransform.position = new Vector3(fieldCenter.x, fieldCenter.y + distanceBetweenFieldAndCamera, fieldCenter.z);
+            var toFieldDirection = (fieldCenter - cameraTransform.position).normalized;
 
-            mainCamera.rotation = Quaternion.Euler(Quaternion.LookRotation(toFieldDirection).eulerAngles + additionalRotation);
+            cameraTransform.rotation = Quaternion.Euler(Quaternion.LookRotation(toFieldDirection).eulerAngles + additionalRotation);
             lightOnScene.rotation = Quaternion.LookRotation(toFieldDirection);
         }
     }
